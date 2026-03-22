abe685e2205485f19fa56208f5f885b810f5fbac
KasjanK <kowalczykkasjan@gmail.com>
2026-03-22 18:08:52 +0100

    style: only show start button if player is the owner of the room

--- main.go
+++ main.go
@@ -93,7 +93,6 @@ func main() {
 	mux.Handle("/static/", http.FileServer(http.FS(staticFS)))
 	
 	//TODO:
-	// - GET PLACE IN MATCH HISTORY
 	// - reassign owner when owner leave
 	// - remove ready function
 	// - only show start button for owner of room
